58d1db9b03b6fb88cafd5880d2660946cc8d8027
chiutailin <curlyajiu@gmail.com>
2021-07-11 21:41:26 +0800

    feat: Hover and highlight
    The cursor hover on a node will highlight the node and its adjacency links and nodes.
    Add fade function to fade the nodes and links when the mouse event occurs.
    The faded objects is provided by the linkedById which contains the link relationship between each nodeId.

--- src/UndirectedGraph.js
+++ src/UndirectedGraph.js
@@ -39,16 +39,15 @@ function UndirectedGraph({ data }) {
         .domain([0, 1])
         .range(["green", "red"])
 
-      const height = 1100
-
-      const width = 960
+      const height = 1080
+      const width = 1920
 
       const links = data.links.map(d => Object.create(d));
       const nodes = data.nodes.map(d => Object.create(d));
 
       const simulation = d3.forceSimulation(nodes)
-        .force("link", d3.forceLink(links).id(d => d.id))
-        .force("charge", d3.forceManyBody())
+        .force("link", d3.forceLink(links).id(d => d.nodeId).strength(d => d.value * 0.04))
+        .force("charge", d3.forceManyBody().strength(-100))
         .force("x", d3.forceX())
         .force("y", d3.forceY());
 
@@ -56,11 +55,11 @@ function UndirectedGraph({ data }) {
 
       const link = svg.append("g")
         .attr("stroke", "#999")
-        .attr("stroke-opacity", 0.6)
+        .attr("stroke-opacity", 0.7)
         .selectAll("line")
         .data(links)
         .join("line")
-        .attr("stroke-width", d => Math.sqrt(d.value));
+        .attr("stroke-width", d => d.value ** -1.5);
 
       const node = svg.append("g")
         .attr("stroke", "#fff")
@@ -68,23 +67,49 @@ function UndirectedGraph({ data }) {
         .selectAll("circle")
         .data(nodes)
         .join("circle")
-        .attr("r", 7)
+        .attr("r", 12)
         .attr("fill", d => color(d.change))
-        .call(drag(simulation));
+        .attr("opacity", 1)
+        .attr("id", d => d.nodeId)
+        .call(drag(simulation))
+        .on("mouseover.fade", fade(0.1, 0.1))
+        .on("mouseout.fade", fade(1, 0.7));
+        
+        node.append("title")
+        .text(d => d.nodeId + "\n" + d.group);
 
-      node.append("title")
-        .text(d => d.nodeId + " " + d.group);
+      const linkedById = {};
+      data.links.forEach(d => {
+        linkedById[`${d.source},${d.target}`] = 1;
+      });
+
+      function isConnected(a, b) {
+        return linkedById[`${a},${b}`] || linkedById[`${b},${a}`] || a === b;
+      }
+
+      function fade(opacity1, opacity2) {
+        return d => {
+          node.style('stroke-opacity', function (o) {
+            const thisOpacity = isConnected(d.path[0].id, o.nodeId) ? 1 : opacity1;
+            this.setAttribute('fill-opacity', thisOpacity);
+            return thisOpacity;
+          });
+
+          link.style('stroke-opacity', o => (data.nodes[o.source.index].nodeId === d.path[0].id || data.nodes[o.target.index].nodeId === d.path[0].id ? 1 : opacity2));
+
+        };
+      }
 
       simulation.on("tick", () => {
         link
-          .attr("x1", d => d.source.x)
-          .attr("y1", d => d.source.y)
-          .attr("x2", d => d.target.x)
-          .attr("y2", d => d.target.y);
+          .attr("x1", d => d.source.x = Math.max(-width / 2, Math.min(width / 2, d.source.x)))
+          .attr("y1", d => d.source.y = Math.max(-height / 2, Math.min(height / 2, d.source.y)))
+          .attr("x2", d => d.target.x = Math.max(-width / 2, Math.min(width / 2, d.target.x)))
+          .attr("y2", d => d.target.y = Math.max(-height / 2, Math.min(height / 2, d.target.y)));
 
         node
-          .attr("cx", d => d.x)
-          .attr("cy", d => d.y);
+          .attr("cx", d => d.x = Math.max(-width / 2, Math.min(width / 2, d.x)))
+          .attr("cy", d => d.y = Math.max(-height / 2, Math.min(height / 2, d.y)));
       });
     }
   );
@@ -94,8 +119,8 @@ function UndirectedGraph({ data }) {
     <svg
       ref={ref}
       style={{
-        height: 500,
-        width: "100%",
+        height: "80%",
+        width: "80%",
         marginRight: "0px",
         marginLeft: "0px",
       }}
